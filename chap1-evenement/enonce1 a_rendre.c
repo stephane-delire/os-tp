f98ac9be1ed0722860f3c14e5e5a5aaeb92954c4
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2024-03-24 11:27:55 +0100

    Ajout de l'entete

--- chap1-evenement/enonce1 a_rendre.c
+++ chap1-evenement/enonce1 a_rendre.c
@@ -1,3 +1,13 @@
+/******************************************************************************
+* S-INFO-054 --- Solution pour Travail 01                                     *
+* Groupe 05                                                                   *
+* Membres:                                                                    *
+* - Amory Delcampe                                                            *
+* - Nathan Rombaut                                                            *
+* - Guy Gillain                                                               *
+* - Stéphane Delire                                                           *
+******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
